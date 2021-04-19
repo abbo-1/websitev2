6485a9d22603c19a7bf41b13a7272cd7ad472e97
abbo-1 <abbott.kevinj@gmail.com>
2021-04-19 17:39:15 -0500

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar.js'
 import isSticky from './components/useSticky.js'
 import Greetings from './components/Greetings.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -3,10 +3,10 @@ import coffeepic from '../images/photo2.png'
 function Greetings() {
     return (
         <div id="greetingDiv">
-            <div class="left">
-                <img src={coffeepic} id="coffeepic" alt="Logo" />
+            <div class="left" className="w-50  d-inline-block">
+                <img src={coffeepic}  id="coffeepic" alt="Logo" />
             </div>
-            <div class="right">
+            <div class="right" className="w-50  d-inline-block">
                 <p id="greetingsText">Greetings!</p>
                 <p id="text">I'm Kevin- a full-stack developer based in Houston, Texas. I know that there are countless developer websites and portfolios out there, so whatever brought you here, I offer a sincere thank you for visiting. If you have any questions please don't hesitate to reach out to me!</p>
                 <button id="resumeBTN">Résumé</button>
