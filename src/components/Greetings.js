03471d349965bd6321ac8cbb4730d81424b99951
abbo-1 <abbott.kevinj@gmail.com>
2021-04-20 15:44:50 -0500

    implemented react bootstrap on greetings component

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,17 +1,20 @@
 import coffeepic from '../images/photo2.png'
+import { Row, Col, Alert, Container} from "react-bootstrap";
 
 function Greetings() {
     return (
-        <div id="greetingDiv">
-            <div class="left" className="w-50  d-inline-block">
+        <Container>
+        <Row id="greetingDiv">
+            <Col className="w-50  d-inline-block">
                 <img src={coffeepic}  id="coffeepic" alt="Logo" />
-            </div>
-            <div class="right" className="w-50  d-inline-block">
+            </Col>
+            <Col className="w-50  d-inline-block">
                 <p id="greetingsText">Greetings!</p>
                 <p id="text">I'm Kevin- a full-stack developer based in Houston, Texas. I know that there are countless developer websites and portfolios out there, so whatever brought you here, I offer a sincere thank you for visiting. If you have any questions please don't hesitate to reach out to me!</p>
                 <button id="resumeBTN">Résumé</button>
-            </div>
-        </div>
+            </Col>
+        </Row>
+        </Container>
     )
 }
 
