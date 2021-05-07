c390db9f3bb45810546d0248e01a9eefdd5226ae
abbo-1 <abbott.kevinj@gmail.com>
2021-05-07 18:06:02 -0500

    fixed resume button

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -12,6 +12,7 @@ function Greetings() {
                 <p id="greetingsText">Greetings!</p>
                 <p id="text">I'm Kevin- a full-stack developer based in Houston, Texas. I know that there are countless developer websites and portfolios out there, so whatever brought you here, I offer a sincere thank you for visiting. If you have any questions please don't hesitate to reach out to me!</p>
                 <button id="resumeBTN">Résumé</button>
+                <a href="documents/Abbott Resume 2020.pdf" target="_blank" id="resumeBTN">Résumé<a></a>
                 {/* I went with the fancy spelling because otherwise it felt like it needed a "RECORD" and "FAST FORWARD" button next to it. */}
             </Col>
         </Row>
