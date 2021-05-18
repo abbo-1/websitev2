9a305d25affd191583b754f9a8281de6a46bf01f
abbo-1 <abbott.kevinj@gmail.com>
2021-05-18 15:06:39 -0500

    more styling

--- src/components/Background.js
+++ src/components/Background.js
@@ -27,15 +27,22 @@ function Background() {
         </Row>
         <Row>
         <Col xs={12} md={6}>
-            <div class="col-md-6">
-				<p class="aboutInfo centerThis">My passion for technology was born while watching my older brother build and fix computers. With his guidance, I built my first PC in 7th grade. Though my primary motivation was the ability to play games like Starcraft, Max Payne, Unreal Tournament, and Fallout 2 - it ended up being the foundation for a life long passion for technology.</p>
+            <div>
+            <div class="yearTitle">
+                2001
+            </div>
+				<p class="aboutInfo centerThis">
+                    My passion for technology took root while watching my older brother build and fix computers. Under his guidance, I built my first PC in 7th grade. 
+                <p class="break" />
+                <p class="break" />
+                    Though my primary motivation at the time was merely to be able to play games like Starcraft, Max Payne, Unreal Tournament, and Fallout 2 - it ended up being the foundation for a life long passion for technology.
+                </p>
 		    </div>
         </Col>
         <Col xs={12} md={6}>
             <img class="img-fluid rounded" src={twoThouOne} alt="" />
 				<div>
-                <h2>2001</h2>
-				<p>Me with my first computer. <h4>Note: the super cool, and not at all nerdy, signed George Takei photo in the background.</h4></p>
+				<p class="turnItWhite">With my first computer.</p>
 				</div>
         </Col>
         </Row>
@@ -44,12 +51,16 @@ function Background() {
         <Col xs={12} md={6}>
         <img class="img-fluid rounded" src={twoThouFive} alt="" />
 			<div>
-                <h2>2005</h2>
-                <p>Me (left) and my brother. <h4>Changing out a network card if I recall correctly.</h4></p>
+            <p class="turnItWhite">
+                Me (left) and my brother replacing a network card. 
+            </p>
 		    </div>
         </Col>
         <Col xs={12} md={6}>
-        <div class="col-md-6">
+        <div>
+        <div class="yearTitle">
+                2005
+            </div>
 				<p class="aboutInfo">PC gaming opened the door to technology, but once I went through it there was no turning back. In high school I tested for my A+ certification and enrolled in networking classes as electives. I also got my first taste of web development, learning basic HTML and CSS.</p>
 		</div>
         </Col>
@@ -57,22 +68,32 @@ function Background() {
         <Divider class="divider" variant="middle"/>
         <Row>
         <Col xs={12} md={6}>
-            <div class="col-md-6 align-self-center">
-				<p class="aboutInfo">One of my very first jobs out of high school was at a local community college. where I managed a computer lab. I honed my ability to diagnose, fix, and maintain three dozen machines. As I got older I found other interests- eventually earning my Bachelor's degree, where I graduated with a 4.0. I invested my spare time in learning to code through the wealth of online resources and a coding program where I was able to learn from industry veterans.</p>
+            <div class="yearTitle">
+                2008
+            </div>
+            <div>
+				<p class="aboutInfo">One of my very first jobs out of high school was at a local community college where I managed a computer lab. Here I honed my ability to diagnose, fix, and maintain over three dozen machines. 
+                <p class="break" />
+                <p class="break" />
+                Eventually I earned my Bachelor's degree at University of Houston Downtown, where I graduated with a 4.0.</p>
 		    </div>
         </Col>
         <Col xs={12} md={6}>
             <img class="img-fluid rounded" src={twoThouEight} alt="" />
 				<div>
-                <h2>2008</h2>
-				<p>									
-                Running the lab</p>
+                <p class="turnItWhite">					
+                Running the lab
+                </p>
 				</div>
         </Col>
         </Row>
         <Divider class="divider" variant="middle"/>
         <Row>
-        <p class="aboutInfo">Obviously that's an incredibly truncated version of my life. In the decade+ that has elapsed- I went to college, got a 'just-to-get-by' job, married, adopted a dog, and sought distraction from the existential terror and dred that comes from our fleeting mortal existance. Last year, I decided to turn my hobby into a career and commited myself to learning everything I can about web development.</p>
+        <p class="aboutInfo">In the decade+ that has elapsed- I got a 'just-to-get-by' job, got married, adopted a rescue dog,
+        {/* sought distraction from the existential terror and dred that comes from our fleeting mortal existance ,  */} and moved into my first house.
+        <p class="break" />
+        <p class="break" />
+        Last year, I decided to commit myself to turning this lifelong hobby and interest into a career. I invested my spare time in learning to code using the wealth of online resources and enrolled myself a coding program where I was able to learn from industry veterans.</p>
         </Row>
         </Container>
       )
