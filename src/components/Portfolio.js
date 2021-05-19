defb58a39b8a4a6b8c2fb87e2ee5bc8322248c4a
abbo-1 <abbott.kevinj@gmail.com>
2021-05-19 17:47:20 -0500

    adding portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,17 +1,28 @@
 import Divider from '@material-ui/core/Divider';
+import { Row, Col, Container, Button } from "react-bootstrap";
 
 function Portfolio() {
 
 
 return (
+        <Container>
+        <Row>
+        <Col xs={11} md={10}>
+            <div class="aboutInfoTitle">
+				<h2>Things I've Made</h2>
+            </div>
+        </Col>
+        <Col xs={1} md={2}>
+            <Button id="xBtn" variant="outline-dark" >CLOSE</Button>
+        </Col>
+        </Row>
 
-    <div>
 
-        <p>TESTt</p>
+
         <Divider class="divider" variant="middle"/>
-        <p>test2</p>
+      test
+        </Container>
 
-    </div>
     )
 }
 
