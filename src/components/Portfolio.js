0881ed87a544c84bd1ff83660161385eccdd970e
abbo-1 <abbott.kevinj@gmail.com>
2021-05-21 14:42:50 -0500

    filling out portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,7 @@ import Divider from '@material-ui/core/Divider';
 import { Row, Col, Container, Button } from "react-bootstrap";
 
 import ThisPage from "../images/portfolio/thiswebsite.png"
+import FamPage from "../images/portfolio/familyweb.png"
 
 function Portfolio() {
 
@@ -23,24 +24,48 @@ return (
 			<p class="aboutInfo">Here are a few of the projects i've worked on.</p>
 		</div>
         </Row>
+
         <Divider class="divider" variant="middle"/>
+
         <Row>
-        <div class="yearTitle">
-    This Website
+        <Col xs={12} md={12}>
+        <div class="portfolioTitle">
+        This Website
         </div>
-        <Col xs={12} md={6}>
-        <p class="aboutInfo">I designed this website- it's not a template! I used React, React-bootstrap, Material-ui, and many other self-designed elements. The original iteration of this website was much simpler as it was made in pure javascript. I also wanted an excuse to practice hooks. My goal was to create something simple, clean, and intuitive. I'm happy with how it's turned out- particularly the buttons on the main page that will copy my related web addresses directly to the clipboard and give an alert.</p>
         </Col>
         <Col xs={12} md={6}>
-        <img src={ThisPage} alt="This Website" />
+        <img src={ThisPage} class="portfolioPic" alt="A screenshot of the webpage you're on right now! Very Inception, I know." />
         <Button size="large" class="portfolioBtn" href="https://www.kevinjabbott.com" target="_blank">LINK</Button>
         <Button size="large" class="portfolioBtn" href="https://github.com/abbo-1/websitev2" target="_blank">CODE</Button>
+        </Col>
+        <Col xs={12} md={6}>
+        <p class="aboutInfo">I designed this website- it's not a template! I used React, React-bootstrap, Material-ui, and many other self-designed elements. The original iteration of this website was much simpler as it was made in pure javascript. I also was looking for excuses to practice hooks. 
+        <p class="break" />
+        <p class="break" />
+        My goal was to create something simple, clean, and intuitive. I'm happy with how it's turned out- particularly the buttons on the main page that will copy my related web addresses directly to the clipboard and give an alert.</p>
         </Col>
         </Row>
 
         <Divider class="divider" variant="middle"/>
 
-
+        <Row>
+        <Col xs={12} md={12}>
+        <div class="portfolioTitle">
+        Family History Website
+        </div>
+        </Col>
+        <Col xs={12} md={6}>
+        <img src={FamPage} class="portfolioPic" alt="A screenshot of a family history webpage" />
+        <Button size="large" class="portfolioBtn" href="https://www.richardsonfamilyhistory.org/" target="_blank">LINK</Button>
+        <Button size="large" class="portfolioBtn" href="https://github.com/abbo-1/familywebsite2" target="_blank">CODE</Button>
+        </Col>
+        <Col xs={12} md={6}>
+        <p class="aboutInfo">I designed this website- it's not a template! I used React, React-bootstrap, Material-ui, and many other self-designed elements. The original iteration of this website was much simpler as it was made in pure javascript. I also was looking for excuses to practice hooks. 
+        <p class="break" />
+        <p class="break" />
+        My goal was to create something simple, clean, and intuitive. I'm happy with how it's turned out- particularly the buttons on the main page that will copy my related web addresses directly to the clipboard and give an alert.</p>
+        </Col>
+        </Row>
 
         </Container>
 
